964e04da2ed9b54f071f8398ae5a94a97e61c35f
Ryan Cai <159085486+RyanCai3@users.noreply.github.com>
2025-02-20 04:02:28 +0000

    deep.py

--- 1-Conditionals/Problems/deep.py
+++ 1-Conditionals/Problems/deep.py
@@ -1,4 +1,4 @@
-#Promts the user for input for the answer to the Great Question of Life, the Universe and Everything
+#Prompt the user for input for the answer to the Great Question of Life, the Universe and Everything
 user_input = input("What's the answer to the Great Question of Life, the Universe and Everything? ")
 
 #Return "yes" if the user typed in the correct answer, and "no" if they didn't
