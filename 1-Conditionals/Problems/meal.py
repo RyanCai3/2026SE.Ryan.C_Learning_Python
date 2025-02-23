b95ebd7a0791296a03f5e048816f4b90d55e1189
Ryan Cai <159085486+RyanCai3@users.noreply.github.com>
2025-02-23 07:45:58 +0000

    Modified meal.py

--- 1-Conditionals/Problems/meal.py
+++ 1-Conditionals/Problems/meal.py
@@ -1,25 +1,25 @@
 def main():
 # Prompt the user for a time input in 24-hour time format and convert to hours
-	time = input("What's the time? (HH:MM) ")
-	new_time = convert(time)
+    time = input("What's the time? (HH:MM) ")
+    new_time = convert(time)
 
 # Check the time and print out what meal it is
-	if 7 <= new_time <= 8:
-		print("Breakfast time")
-	elif 12 <= new_time <= 13:
-		print("Lunch time")
-	elif 18 <= new_time <= 19:
-		print("Dinner time")
+    if 7 <= new_time <= 8:
+        print("Breakfast time")
+    elif 12 <= new_time <= 13:
+        print("Lunch time")
+    elif 18 <= new_time <= 19:
+        print("Dinner time")
 
 def convert(time):
 # Split the input time into hours and minutes
-	hours, minutes, = time.split(":")
+    hours, minutes, = time.split(":")
 
 # Convert hours and minutes into integers
-	hours = int(hours)
-	minutes = int(minutes)
+    hours = int(hours)
+    minutes = int(minutes)
 
 # Convert minutes to a fraction of an hour
-	return hours + minutes / 60
+    return hours + minutes / 60
 
 main()
