2e3044a9e04f8078f7ad65fe8c932c119b0d9e5b
Ryan Cai <159085486+RyanCai3@users.noreply.github.com>
2025-05-15 23:56:39 +0000

    Problem 1

---
+++ 8-OOP/Problems/seasons.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+def main():
+	...
+
+if __name__ == "__main__":
+	main()
--- 8-OOP/Workshop_Activities/wizard.py
+++ 8-OOP/Workshop_Activities/wizard.py
@@ -4,23 +4,17 @@ class Wizard:
             raise ValueError("Missing name")
         self.name = name
 
-    ...
-
 class Student(Wizard):
     def __init__(self, name, house):
         super().__init__(name)
         self.name = name
         self.house = house
 
-    ...
-
 class Professor(Wizard):
     def __init__(self, name, subject):
         super().__init__(name)
         self.name = name
         self.subject = subject
-
-    ...
 
 wizard = Wizard("Albus")
 student = Student("Harry", "Gryffindor")
